a2865d01ac7a76556c97ed221f0a3b2255550b70
johndharrison <johndharrison0@gmail.com>
2016-12-31 15:54:23 -0800

    Update test

--- tests/testthat/test-parse_version.R
+++ tests/testthat/test-parse_version.R
@@ -3,8 +3,11 @@ context("parse_version")
 test_that("canParseVersions", {
   version <- c("1.2.3-test","1.2.3-321", "1.2.3-test.1", "1.2.3-1.test",
                "1.2.3-test.123456", "1.2.3-123456.test",
-               "1.2.3-1.a.22.bb.333.ccc.4444.dddd.55555.fffff")
+               "1.2.3-1.a.22.bb.333.ccc.4444.dddd.55555.fffff",
+               "1.2.3-alpha.1.2.3+build.314")
   out <- parse_version(version)
-  expect_identical(sum(res), -7L)
-
+  expect_s3_class(out, "svlist")
+  expect_true(all(vapply(out, class, character(1L)) == "svptr"))
+  expect_identical(out[[8]]$build, "build.314")
+  expect_identical(out[[8]]$prerelease, "alpha.1.2.3")
 })
