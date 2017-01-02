e9a692e615e5ffedec4df2296bf42d0a1b4a17b6
johndharrison <johndharrison0@gmail.com>
2017-01-02 08:39:55 -0800

    Update parse_version examples

--- R/parse_version.R
+++ R/parse_version.R
@@ -11,6 +11,28 @@
 #' max(semver)
 #' ver124a <- semver[semver > "1.2.4-alpha"]
 #' sort(ver124a)
+#' # compare versions
+#' semver[[1]] < semver[[2]]
+#'
+#' # compare against a version
+#' semver > semver[[1]]
+#'
+#' # compare against a character string
+#' semver > "1.7.2"
+#'
+#' # sort versions
+#' sort(semver)
+#' order(semver)
+#' rank(semver)
+#'
+#' # get summary statistics
+#' min(semver)
+#' max(semver)
+#' range(semver)
+#'
+#' # coerce versions
+#' as.character(semver)
+#' as.data.frame(semver)
 
 parse_version <- function(version){
   assertthat::assert_that(is.character(version))
