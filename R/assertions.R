8fa76c7b0e2ce7e4f2efe7943454aedfb9d4fc60
johndharrison <johndharrison0@gmail.com>
2017-01-04 12:47:05 -0800

    Add assertions

--- R/assertions.R
+++ R/assertions.R
@@ -5,3 +5,19 @@ is_character <- function(x){
 assertthat::on_failure(is_character) <- function(call, env) {
   paste0(deparse(call$x), " should be an character vector.")
 }
+
+is_string <- function(x) {
+  is.character(x) && length(x) == 1 && !is.na(x)
+}
+
+assertthat::on_failure(is_string) <-  function(call, env) {
+  paste0(deparse(call$x), " is not a string")
+}
+
+is_integer <- function(x){
+  is.integer(x)
+}
+
+assertthat::on_failure(is_integer) <- function(call, env) {
+  paste0(deparse(call$x), " should be an integer value.")
+}
