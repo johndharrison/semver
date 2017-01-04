8eb24f91a18376b3b11f3e1c84ee77c0d7a177ef
johndharrison <johndharrison0@gmail.com>
2017-01-04 15:39:27 -0800

    Add set_version method for "svlist"

--- R/set_version.R
+++ R/set_version.R
@@ -9,8 +9,12 @@
 #' @export
 #'
 #' @examples
+#' semver <- parse_version(c("1.2.3", "1.2.4-alpha+bld1"))
+#' semver[[1]] <- set_version(semver[[1]], "major", 2L)
+#' set_version(semver, c("prerelease", "build"), list("hello"))
+#' set_version(semver, c("prerelease"), list("hello", "world"))
 
-set_version <- function(x, ...){
+set_version <- function(x, field, value, ...){
   UseMethod("set_version")
 }
 
@@ -25,7 +29,7 @@ set_version <- function(x, ...){
 #'     the "svlist" class value is a list detailing the value to set for
 #'     each list member.
 set_version.svptr <- function(x, field = c("major", "minor", "patch",
-                                       "prerelease", "build"), value){
+                                       "prerelease", "build"), value, ...){
   field <- match.arg(field)
   fieldIdx <- switch(field,
                   major = 1L,
@@ -35,9 +39,16 @@ set_version.svptr <- function(x, field = c("major", "minor", "patch",
                   build = 5L
   )
   if(fieldIdx < 4L){
-    assertthat::assert_that(is_integer(value))
+    assertthat::assert_that(is_integer_sv(value))
   }else{
-    assertthat::assert_that(is_string(value))
+    assertthat::assert_that(is_string_sv(value))
   }
   set_ptr(x, fieldIdx, value[1])
+}
+
+#' @export
+#' @rdname set_version
+set_version.svlist <- function(x, field, value, ...){
+  assertthat::assert_that(is_list(value))
+  Map(set_version, x = x, field = field, value = value)
 }
