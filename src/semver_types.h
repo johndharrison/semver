051e9b6a6bd4992af559aa55ae3c7a9220c59409
johndharrison <johndharrison0@gmail.com>
2017-01-04 12:47:32 -0800

    Redefine typedef and add new

--- src/semver_types.h
+++ src/semver_types.h
@@ -1,8 +1,18 @@
 #include <Rcpp.h>
 #include "semver200.h"
 
-inline void svptr_finalizer(sv_version::Semver200_version* svptr) {
+typedef sv_version::Semver200_version sver200;
+inline void svptr_finalizer(sver200* svptr) {
   delete svptr;
 }
+typedef Rcpp::XPtr<sver200, Rcpp::PreserveStorage, svptr_finalizer> XPtrsver200;
 
-typedef Rcpp::XPtr<sv_version::Semver200_version,Rcpp::PreserveStorage, svptr_finalizer> semver200;
+typedef sv_version::Basic_version<
+  sv_version::Semver200_parser,
+  sv_version::Semver200_comparator,
+  sv_version::Semver200_modifier
+  > bver;
+inline void bvptr_finalizer(bver* bvptr) {
+  delete bvptr;
+}
+typedef Rcpp::XPtr<bver, Rcpp::PreserveStorage, bvptr_finalizer> XPtrbver;
